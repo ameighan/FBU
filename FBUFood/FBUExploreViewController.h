fd136108ebf6cf4da841bbf0aaa5568d839c925f
Jennifer Zhang <jenniferjzhang@fb.com>
2014-07-22 10:45:43 -0700

    New and improved story board
    Reviewers: christineyen
    
    Reviewed By: christineyen
    
    CC: umag, ameighan, nlutsenko
    
    Differential Revision: https://phab.parse.com/D5414

---
+++ FBUFood/FBUExploreViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBUExploreViewController.h
+//  FBUFood
+//
+//  Created by Jennifer Zhang on 7/22/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBUExploreViewController : UIViewController
+
+@end
---
+++ FBUFood/FBUFood/FBUMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBUMapViewController.h
+//  FBUFood
+//
+//  Created by Jennifer Zhang on 7/22/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBUMapViewController : UIViewController
+
+@end
